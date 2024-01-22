12a1397608c39ac8012a85f96f56493854593668
Justin <web@jstn.ly>
2024-01-22 23:56:02 +0700

    Refactor post-categories component and update nginx configuration

--- app/components/post-categories.tsx
+++ app/components/post-categories.tsx
@@ -6,11 +6,15 @@ const PostCategories: FC<{ categories: string[] }> = ({ categories }) => {
   return (
     <div className="flex items-center gap-x-2 text-xs">
       {categories.map((category: string) => (
-        <Link key={category} href={`/category/${category.toLowerCase()}`}>
-          <Badge color="green" className="border font-xs border-green-400/30">
-            {category}
-          </Badge>
-        </Link>
+        // <Link key={category} href={`/category/${category.toLowerCase()}`}>
+        <Badge
+          key={category}
+          color="green"
+          className="border font-xs border-green-400/30"
+        >
+          {category}
+        </Badge>
+        // </Link>
       ))}
     </div>
   );
